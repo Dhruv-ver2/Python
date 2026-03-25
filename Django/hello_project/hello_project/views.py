20b1a05dbb06ddffc625d56fcf61df7d210d5c7d
Dhruv Vaishnav <dhruv2vaishnav@gmail.com>
2026-03-25 23:03:14 +0530

    Editing the project

--- Django/hello_project/hello_project/views.py
+++ Django/hello_project/hello_project/views.py
@@ -1,4 +1,10 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 def fun(request):
-    return HttpResponse("Dhruv is learning backend with Django")
+    # Example data to send to the template
+    context = {
+        "time": datetime.datetime.now().strftime("%H:%M:%S"),
+    }
+    return render(request, "index.html", context)
