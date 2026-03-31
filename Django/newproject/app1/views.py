c16cc518add810d833d526fdedce3ff75b430145
Dhruv Vaishnav <dhruv2vaishnav@gmail.com>
2026-03-31 18:42:30 +0530

    Added a new project

--- Django/newproject/app1/views.py
+++ Django/newproject/app1/views.py
@@ -1,7 +1,17 @@
-#app1/views.py
 from django.shortcuts import render
 
+# Your existing view
 def fun(request):
-    return render(request, 'sample.html', {'message': 'Hello from App1!'})
+    return render(request, 'sample.html', {'message': 'Welcome to the Home Page!'})
 
+# NEW: Profile View
+def profile_view(request):
+    context = {
+        'name': 'Your Name',
+        'hobby': 'Learning Django'
+    }
+    return render(request, 'profile.html', context)
 
+# NEW: Contact View
+def contact_view(request):
+    return render(request, 'contact.html')
