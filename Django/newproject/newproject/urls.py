b992ebf2726154d9a7a41ce2c6faacf4f35aa8dc
Dhruv Vaishnav <dhruv2vaishnav@gmail.com>
2026-03-31 18:21:03 +0530

    Added a new project

--- Django/newproject/newproject/urls.py
+++ Django/newproject/newproject/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import fun
 
 urlpatterns = [
-    path('new/', fun),
+    path('app1/new/', include('app1.urls')),
 ]
